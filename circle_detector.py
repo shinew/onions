7cb9fe5ce1464685c0d4c039a2a3d3d74d2832ee
Zihao Zhang <zzh8829@gmail.com>
2018-01-06 18:30:05 -0500

    Add title

--- circle_detector.py
+++ circle_detector.py
@@ -4,27 +4,33 @@ import matplotlib.pyplot as plt
 import time
 
 class CircleDetector:
+    def __init__(self):
+        self.resize_x = 300
 
     def detect(self, path):
         img = cv2.imread(path,0)
         img = cv2.medianBlur(img,5)
-        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                                    param1=50,param2=30,minRadius=0,maxRadius=0)
-        circles = np.uint16(np.around(circles))
-        return circles[0,0]
+        height, width = img.shape[:2]
 
-    def debug(self, path):
-        img = cv2.imread(path,0)
-        img = cv2.medianBlur(img,5)
-        cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-        circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+        ratio = max(height//self.resize_x, 1)
+        small = cv2.resize(img, (int(width / ratio), int(height / ratio)), interpolation = cv2.INTER_CUBIC)
+        circles = cv2.HoughCircles(small,cv2.HOUGH_GRADIENT,1,20,
                                     param1=50,param2=30,minRadius=0,maxRadius=0)
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            # draw the outer circle
-            cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-            # draw the center of the circle
-            cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-        plt.imshow(cimg)
-        plt.show()
+        return (circles[0,0] * ratio).astype(int)
+
+if __name__ == '__main__':
+    img = cv2.imread('onion2.jpg',0)
+    img = cv2.medianBlur(img,5)
+    cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                                param1=50,param2=30,minRadius=0,maxRadius=0)
+    circles = np.uint16(np.around(circles))
+    for i in circles[0,:]:
+        # draw the outer circle
+        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    plt.imshow(cimg)
+    plt.show()
 
--- main.py
+++ main.py
@@ -40,18 +40,20 @@ def is_onion(response):
 
 import circle_detector
 
+img = cv2.imread(file_name)
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 if is_onion(response):
     print('onion')
-
+    cv2.putText(img,"Onion", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
     detector = circle_detector.CircleDetector()
     circle = detector.detect(file_name)
-
-    img = cv2.imread(file_name)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
     cv2.circle(img,(circle[0],circle[1]),circle[2],(0,255,0),2)
     # draw the center of the circle
     cv2.circle(img,(circle[0],circle[1]),2,(0,0,255),3)
-    plt.imshow(img)
-    plt.show()
 else:
     print('not onion')
+    cv2.putText(img,"NOT Onion", (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2)
+
+plt.imshow(img)
+plt.show()
